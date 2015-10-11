8c172508bef79785b32a8a82149fe933043efa12
clux <analsandblaster@gmail.com>
2015-10-12 00:44:29 +0100

    test library output as well and add coverage

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 [![npm status](http://img.shields.io/npm/v/mddocs.svg)](https://www.npmjs.org/package/mddocs)
 [![build status](https://secure.travis-ci.org/clux/mddocs.svg)](http://travis-ci.org/clux/mddocs)
 [![dependency status](https://david-dm.org/clux/mddocs.svg)](https://david-dm.org/clux/mddocs)
+[![coverage status](http://img.shields.io/coveralls/clux/mddocs.svg)](https://coveralls.io/r/clux/mddocs)
 
 An unconfigurable docs generator from markdown with syntax highlighting. Designed for personal ease of use. Examples to come, but you likely don't want this.
 
--- test/md.test.js
+++ test/md.test.js
@@ -1,10 +1,21 @@
 var test = require('bandage');
 var fs = require('co-fs');
+var mddoc = require('..');
 
-test('api.md', function *(t) {
-  var html = yield fs.readFile('./test/api.html', 'utf8');
+var verifyTestMd = function (t, html) {
   t.ok(html.indexOf('href="https://woot.github.io/testmod">') >= 0, 'canonical');
   t.ok(html.indexOf('<span class="hljs-number">') >= 0, 'markdown');
   t.ok(html.indexOf('<title>Testmod Documentation v1.0.0</title>') >= 0, 'title');
   t.ok(html.indexOf('<script>hljs.initHighlightingOnLoad();</script>') >= 0, 'syntx');
+};
+
+test('cli output', function *(t) {
+  var html = yield fs.readFile('./test/api.html', 'utf8');
+  verifyTestMd(t, html);
+});
+
+test('lib output', function *(t) {
+  var input = yield fs.readFile('./test/api.md', 'utf8');
+  var html = yield *mddoc(input, { user: 'woot', name: "testmod", version: "1.0.0" });
+  verifyTestMd(t, html);
 });
