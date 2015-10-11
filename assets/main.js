8355a518c46b3fdb681dc4a948b44fb6e16614ed
clux <analsandblaster@gmail.com>
2015-10-12 00:23:18 +0100

    auto-generate toc by scanning the header links - fixes #1

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,29 @@
-console.log('initialized');
+var generateToc = function (children) {
+  var toc = [];
+  var activeLevel = 0;
+
+  Array.prototype.forEach.call(children, function (node) {
+    if (['H1', 'H2', 'H3', 'H4', 'H5'].indexOf(node.nodeName) >= 0) {
+      var level = node.nodeName.slice(1) | 0;
+      var goingUp = level > activeLevel;
+      while (activeLevel !== level) {
+        activeLevel += goingUp ? 1 : -1;
+        toc.push(goingUp ? '<ul>' : '</ul>');
+      }
+      toc.push('<li>' + node.innerText + '</li>');
+      activeLevel = level;
+    }
+  });
+
+  while (activeLevel > 0) {
+    activeLevel -= 1;
+    toc.push('</ul>');
+  }
+  return toc.join('');
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var toc = document.querySelector('#toc');
+  var body = document.querySelector('body');
+  toc.innerHTML = generateToc(body.children);
+});
