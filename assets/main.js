161f20a874c2bc435c9d04bc6efc04b31bec8517
clux <analsandblaster@gmail.com>
2015-10-12 19:09:45 +0100

    add anchors to toc and style it a little bit

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,33 @@
-var generateToc = function (children) {
-  var toc = [];
+var findHeadings = function (children) {
+  return Array.prototype.filter.call(children, function (node) {
+    return ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].indexOf(node.nodeName) >= 0;
+  });
+};
+
+var anchorize = function (text, linktext) {
+  var safelink = text.replace(/\W/g, '_').replace(/_{2,}/g, '_');
+  var idstr = linktext === '#' ? ' id="'+ safelink + '" ': '';
+  return '<a ' + idstr + 'href="#' + safelink + '">' + (linktext || text) + '</a>';
+};
+
+var insertAnchors = function (headings) {
+  headings.forEach(function (hx) {
+    hx.innerHTML = hx.innerText + '<span>' + anchorize(hx.innerText, '#') + '</span>';
+  });
+};
+
+var generateToc = function (headings) {
+  var toc = ['<h2>Table of Contents</h2>'];
   var activeLevel = 0;
 
-  Array.prototype.forEach.call(children, function (node) {
-    if (['H1', 'H2', 'H3', 'H4', 'H5'].indexOf(node.nodeName) >= 0) {
-      var level = node.nodeName.slice(1) | 0;
-      var goingUp = level > activeLevel;
-      while (activeLevel !== level) {
-        activeLevel += goingUp ? 1 : -1;
-        toc.push(goingUp ? '<ul>' : '</ul>');
-      }
-      // TODO: sensible anchor link
-      toc.push('<li><a href=#>' + node.innerText + '</a></li>');
+  headings.forEach(function (hx) {
+    var level = hx.nodeName.slice(1) | 0;
+    var goingUp = level > activeLevel;
+    while (activeLevel !== level) {
+      activeLevel += goingUp ? 1 : -1;
+      toc.push(goingUp ? '<ul>' : '</ul>');
     }
+    toc.push('<li>' + anchorize(hx.innerText) + '</li>');
   });
 
   while (activeLevel > 0) {
@@ -24,6 +39,8 @@ var generateToc = function (children) {
 
 document.addEventListener("DOMContentLoaded", function () {
   var toc = document.querySelector('#toc');
-  var body = document.querySelector('body');
-  toc.innerHTML = generateToc(body.children);
+  var contents = document.querySelector('#apicontent');
+  var headings = findHeadings(contents.children);
+  toc.innerHTML = generateToc(headings);
+  insertAnchors(headings);
 });
